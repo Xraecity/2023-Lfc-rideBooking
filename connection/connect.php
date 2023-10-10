43f7c9b8f8bf08e456ad6afe0ddc7da271c10942
xraecity <xraecity.xcl@yahoo.com>
2023-10-09 21:20:44 -0500

    new reps

--- connection/connect.php
+++ connection/connect.php
@@ -1,8 +1,8 @@
 <?php
 
-define('DB_DSN', 'mysql:host=localhost;dbname=bookride;charset=utf8');
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_DSN', 'mysql:host=us-cluster-east-01.k8s.cleardb.net;dbname=heroku_b4cc250565d2383;charset=utf8');
+define('DB_USER','b9cc45b4749d89');
+define('DB_PASS','0ec0a262');
 
 try{
     $db = new PDO(DB_DSN, DB_USER, DB_PASS);
