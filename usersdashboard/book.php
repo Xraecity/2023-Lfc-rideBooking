44a1b2091ca20f702aa29bc28887f651fd5d92f5
Prince Xrae <49319398+Xraecity@users.noreply.github.com>
2023-10-10 19:32:04 -0500

    Update book.php

--- usersdashboard/book.php
+++ usersdashboard/book.php
@@ -69,7 +69,7 @@ $secondSundayFormatted = $secondSunday->format('l jS M');
                         $ridebookingData = array();
                                         
                         // Fetch ridebooking data from the database
-                        $query = "SELECT * FROM ridebooking ORDER BY created_at DESC";
+                        $query = "SELECT * FROM ridebooking WHERE user_id = :user_id ORDER BY created_at DESC";
                         $dis = ''; // Initialize the message
                         $scheduleEverySunday = false; // Initialize a flag
                         $scheduleBiWeekly = false; // Initialize a flag
